9d191f76b0546a0354de0158769fbdbfcf977c7c
Jim Chen <jimchen@Jims-Air.fios-router.home>
2020-07-18 06:13:56 -0400

    tackled dashboard component with made up data

---
+++ client/src/routes/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+  <p className="footer">© Copyright {new Date().getFullYear()}. All Rights Reserved.</p>
+)};
+
+export default Footer;
---
+++ client/src/routes/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Navbar.css'
+import { NavLink } from 'react-router-dom';
+
+function NavBar () {
+
+    return (
+        <nav>
+            <div>
+                <div className="logo">placeholder for logo</div>
+            </div>
+            <ul className='Navbar-dashboard'>
+                <li><NavLink to='/' className='navLink'> DASHBOARD </NavLink></li>
+                <li><NavLink to='/' className='navLink'> IDEAS </NavLink></li>
+                <li><NavLink to='/' className='navLink'> INBOX </NavLink></li>
+            </ul>
+            <ul className='Navbar-personal'>
+                <li><NavLink to='/' className='navLink2'> About </NavLink></li>
+                <li><NavLink to='/' className='navLink2'> Settings </NavLink></li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default NavBar
