5d700a78087fb8183c43315735576ae670e57aa6
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-07-19 18:20:17 -0400

    styling for nav and feed shell

--- client/src/routes/Navbar.jsx
+++ client/src/routes/Navbar.jsx
@@ -1,26 +1,54 @@
 import React from 'react'
 import './Navbar.css'
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function NavBar () {
 
     return (
-        <nav>
-            <div>
-                <div className="logo">placeholder for logo</div>
-            </div>
-            <ul className='Navbar-dashboard'>
-                <li><NavLink to='/' className='navLink'> DASHBOARD </NavLink></li>
-                <li><NavLink to='/' className='navLink'> IDEAS </NavLink></li>
-                <li><NavLink to='/' className='navLink'> INBOX </NavLink></li>
-                <li><NavLink to='/' className='navLink'> CREATE YOUR IDEA </NavLink></li>
-            </ul>
-            <ul className='Navbar-personal'>
+        <header> 
+        <nav className="navbar fixed-top navbar-expand-sm navbar-light">
+           <a class="navbar-brand" href="/"><img
+        className="Logo"
+        src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595024102/Untitled_2_tcxox8.jpg"
+        alt="Ideate Logo"
+      /></a>
+           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav navbar-light">
+      <a className="nav-item nav-link active" > <span className="sr-only">(current)</span> 
+        <Link to='/' className='navLink'>
+        <div className="togglewrapperrr" data-toggle="collapse" data-target=".navbar-collapse.show"> DASHBOARD </div></Link></a>
+
+        <a className="nav-item nav-link" >
+            <Link to='/' className='navLink'>
+        <div className="togglewrapperrr" data-toggle="collapse" data-target=".navbar-collapse.show">
+             IDEAS 
+             </div>
+             </Link>
+             </a>
+             <a className="nav-item nav-link" >
+                 <Link to='/' className='navLink'>
+                 <div className="togglewrapperrr" data-toggle="collapse" data-target=".navbar-collapse.show">
+                      INBOX 
+                      </div>
+                      </Link>
+                      </a>
+                      <a className="nav-item nav-link" >
+                          <Link to='/' className='navLink'> 
+                          <div className="togglewrapperrr" data-toggle="collapse" data-target=".navbar-collapse.show">
+                              CREATE YOUR IDEA 
+                              </div></Link>
+                              </a>
+            {/* <ul className='Navbar-personal'>
                 <li><NavLink to='/' className='navLink2'> About </NavLink></li>
                 <li><NavLink to='/' className='navLink2'> Settings </NavLink></li>
-            </ul>
+            </ul> */}
+            </div>
+        </div> 
         </nav>
-
+        </header> 
     )
 }
 
