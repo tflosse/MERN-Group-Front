f8fc58d6bd0d42dd39d9c26068701c49f79e5f1f
Tamara Vahinerii Flosse <tflosse@tamaras-mbp.home>
2020-07-20 14:36:15 -0700

    Okta API folder added

---
+++ api/lib/oktaClient.js
@@ -0,0 +1,9 @@
+  
+const okta = require('@okta/okta-sdk-nodejs');
+
+const client = new okta.Client({
+  orgUrl: 'https://dev-353320.okta.com',
+  token: '008oG1VNkwGi8EHGOfknbjGbq_A--jNvMcu0bq0a2i'
+});
+
+module.exports = client;
