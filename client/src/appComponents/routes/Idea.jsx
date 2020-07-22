90428914672dec2664a71dcd38ea33fd2fabd4a6
Jim Chen <jimchen@Jims-iMac.fios-router.home>
2020-07-22 13:32:49 -0400

    ind idea main section rendered

--- client/src/appComponents/routes/Idea.jsx
+++ client/src/appComponents/routes/Idea.jsx
@@ -1,8 +1,42 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios'
 import './Idea.css'; 
+import { usersApi, ideasApi } from '../../apiConfig.js'
 
-function Idea () {
+function Idea (props) {
     const checkmark = '✔';
+    console.log('Idea',props)
+   
+    let ideaId=props.match.params.ideatitle
+    console.log('api add: ', `${ideasApi}/id/${ideaId}`)
+
+    const [ideas, setIdea] = useState({})
+    
+    useEffect(() => {
+        const makeAPICall = async () => {
+          try {
+              const response = await axios(`${ideasApi}/${ideaId}`)
+              console.log('Idea - response', response)
+              setIdea(response.data)
+            } catch (err) {
+              console.error(err)
+            }
+       }
+        makeAPICall()
+      }, [])
+      
+      console.log('keytags',ideas)
+      function keytags(array){
+          if(!array) return <p>Loading...</p>
+          else {
+        const keytagArr=array.map((item)=>(
+            <p className='dashboard-ind-key'>#{item}</p>
+        ))
+        return keytagArr
+          }
+    }
+
+
     let idea={
         creative_idea: {
             title: "coffee when you are done with hygiene",
@@ -41,11 +75,6 @@ function Idea () {
         }
     }
     let authorName= (idea.personal.isAnonymous ? 'Anonymous':idea.personal.username.name)
-    let keytags=idea.creative_idea.keywords.map((tags)=>{
-        return (
-            <p className='dashboard-ind-key'>#{tags}</p>
-        )
-    })
     
     let userComments 
     if(idea.otherUserResponse.comments[0]){
@@ -65,54 +94,54 @@ function Idea () {
     return (
         <>
         <div className="FeedUltimateContainer">
-        <div className="FeedHeaderContainer">
-        <div className="FeedHeader">
-          <img className="FeedHeaderAnimation" src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595116789/Untitled_7_2_vm15aq.png" alt="Flashing Lightbulb"/>      
-        </div>
-        </div>
-        <div className="MainFeed">
-        <div className='dashboard-creative-idea'>
-            <div className='dashboard-title'>
-                <div className="Title-and-votes">
-                <div className="IconsBoxPost">
-                  <div className="PostUpBox">
-                    <i className="material-icons">keyboard_arrow_up</i>
-                    <img className="LightbulbIcon" src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595186577/Untitled_8_1_ymtxrr.png" alt="whole lightbulb"/>
-                  </div>
-                  <div className="PostCountBox">
-                        <p>0</p>
-                  </div>
-                  <div className="PostDownBox">
-                    <i className="material-icons">keyboard_arrow_down</i>
-                    <img className="LightbulbIcon" src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595188165/Untitled_8_2_glwnue.png" alt="Broken lightbulb"/>
-                  </div>
+            <div className="FeedHeaderContainer">
+                <div className="FeedHeader">
+                    <img className="FeedHeaderAnimation" src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595116789/Untitled_7_2_vm15aq.png" alt="Flashing Lightbulb"/>      
                 </div>
-                <h1 className='dashboard-idea-title ideatitle'>{idea.creative_idea.title}</h1>
+            </div>
+            <div className="MainFeed">
+                <div className='dashboard-creative-idea'>
+                    <div className='dashboard-title'>
+                        <div className="Title-and-votes">
+                            <div className="IconsBoxPost">
+                                <div className="PostUpBox">
+                                    <i className="material-icons">keyboard_arrow_up</i>
+                                    <img className="LightbulbIcon" src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595186577/Untitled_8_1_ymtxrr.png" alt="whole lightbulb"/>
+                                </div>
+                                    <div className="PostCountBox">
+                                        <p>{ideas.votes}</p>
+                                    </div>
+                                <div className="PostDownBox">
+                                    <i className="material-icons">keyboard_arrow_down</i>
+                                    <img className="LightbulbIcon" src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595188165/Untitled_8_2_glwnue.png" alt="Broken lightbulb"/>
+                                </div>
+                            </div>
+                            <h1 className='dashboard-idea-title feedtitle'>{ideas.title}</h1>
+                        </div>
+                        <h1 className='dashboard-idea-author'>{ideas.username}</h1>
+                    </div>
+                    <div className='dashboard-desc'>
+                        <p>{ideas.description}</p>
+                        {/* <p>keywords:</p> */}
+                        <div className='dashboard-keytags'>{keytags(ideas.keywords)}</div>
+                    </div>
                 </div>
-                <h1 className='dashboard-idea-author'>{authorName}</h1>
-            </div>
-            <div className='dashboard-desc'>
-                <p>{idea.creative_idea.description}</p>
-                {/* <p>keywords:</p> */}
-                <div className='dashboard-keytags'> {keytags}</div>
-            </div>
-            <div className='dashboard-comments-section'>
-                <div className="BarrierContainer">
-                <img className="CommentBarrier" src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595191527/Untitled_12_taesgk.png" alt="Telephone Wire"/>
+                <div className='dashboard-comments-section'>
+                    <div className="BarrierContainer">
+                        <img className="CommentBarrier" src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595191527/Untitled_12_taesgk.png" alt="Telephone Wire"/>
+                    </div>
+                    <p className='dashboard-comments-title'>Comments</p>
+                    {userComments}
                 </div>
-                <p className='dashboard-comments-title'>Comments</p>
-                {userComments}
+                <div className="AddComment">
+                    <form className="CommentForm">
+                        <p> Anon {checkmark} </p>
+                        <input placeholder="Leave a Comment"/> 
+                        <input placeholder="Add Hashtags"/>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
             </div>
-            <div className="AddComment">
-                <form className="CommentForm">
-                    <p> Anon {checkmark} </p>
-                    <input placeholder="Leave a Comment"/> 
-                    <input placeholder="Add Hashtags"/>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-        </div>
-        </div>
         </div>
         </> 
     )
