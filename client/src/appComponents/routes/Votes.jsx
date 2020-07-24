ba23d1bcb6782533f7d970fdd27a21104f2f5b45
Tamara Vahinerii Flosse <tflosse@tamaras-mbp.home>
2020-07-23 17:39:51 -0700

    Fixed to img lightbulb

--- client/src/appComponents/routes/Votes.jsx
+++ client/src/appComponents/routes/Votes.jsx
@@ -1,96 +1,95 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Idea.css";
 import { usersApi, ideasApi } from "../../apiConfig.js";
 
-const Votes = ({ideaId}) => {
-    console.log(ideaId)
-    const [votes, updateVotes] = useState();
-    const [upvoted, setUpvoted] = useState(false);
-    const [downvoted, setDownvoted] = useState(false);
+const Votes = ({ ideaId }) => {
+  console.log(ideaId);
+  const [votes, updateVotes] = useState();
+  const [upvoted, setUpvoted] = useState(false);
+  const [downvoted, setDownvoted] = useState(false);
 
-    useEffect(() => {
-      const makeAPICall = async () => {
-        try {
-          const response = await axios(`${ideasApi}/${ideaId}`);
-          console.log("Idea - response", response);
-          updateVotes(response.data.votes);
-          console.log(votes)
-        } catch (err) {
-          console.error(err);
-        }
-      };
-      makeAPICall();
-    }, []);
-
-    const handleUpvote = async (event) => {
-        try {
-          const response = await axios(`${ideasApi}/${ideaId}`);
-          updateVotes(response.data.votes +=1)
-          let newData = response.data
-          await axios({
-            url: `${ideasApi}/${ideaId}`,
-            method: "PUT",
-            data: newData
-          })
-        //   setIsUpdated(true)
-        } catch (err) {
-          console.error(err);
-        }
-        setUpvoted(true);
-        setDownvoted(false);
-      console.log('handled Upvote')
+  useEffect(() => {
+    const makeAPICall = async () => {
+      try {
+        const response = await axios(`${ideasApi}/${ideaId}`);
+        console.log("Idea - response", response);
+        updateVotes(response.data.votes);
+        console.log(votes);
+      } catch (err) {
+        console.error(err);
       }
+    };
+    makeAPICall();
+  }, []);
 
-      const handleDownvote = async (event) => {
-        try {
-          const response = await axios(`${ideasApi}/${ideaId}`);
-          updateVotes(response.data.votes -=1)
-          let newData = response.data
-          await axios({
-            url: `${ideasApi}/${ideaId}`,
-            method: "PUT",
-            data: newData
-          })
-        //   setIsUpdated(true)
-        } catch (err) {
-          console.error(err);
-        }
-        setDownvoted(true);
-        setUpvoted(false);
-        console.log('handled Downvote')
-      }
+  const handleUpvote = async (event) => {
+    try {
+      const response = await axios(`${ideasApi}/${ideaId}`);
+      updateVotes((response.data.votes += 1));
+      let newData = response.data;
+      await axios({
+        url: `${ideasApi}/${ideaId}`,
+        method: "PUT",
+        data: newData,
+      });
+      //   setIsUpdated(true)
+    } catch (err) {
+      console.error(err);
+    }
+    setUpvoted(true);
+    setDownvoted(false);
+    console.log("handled Upvote");
+  };
 
+  const handleDownvote = async (event) => {
+    try {
+      const response = await axios(`${ideasApi}/${ideaId}`);
+      updateVotes((response.data.votes -= 1));
+      let newData = response.data;
+      await axios({
+        url: `${ideasApi}/${ideaId}`,
+        method: "PUT",
+        data: newData,
+      });
+      //   setIsUpdated(true)
+    } catch (err) {
+      console.error(err);
+    }
+    setDownvoted(true);
+    setUpvoted(false);
+    console.log("handled Downvote");
+  };
 
-    return (
-        <div className="IconsBoxPost">
-            <div className="PostUpBox">
-            <i className="material-icons"
-            onClick={handleUpvote}>{`${upvoted? "  ":"keyboard_arrow_up"}`}</i>
-            {/* <img
-                className="LightbulbIcon"
-                src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595186577/Untitled_8_1_ymtxrr.png"
-                alt="whole lightbulb"
-            /> */}
-            </div>
-            <div className="PostCountBox">
-            <p>
-                <span class="material-icons">
-                emoji_objects
-                </span> 
-                <strong>{votes}</strong></p>
-            </div>
-            <div className="PostDownBox">
-            <i className="material-icons"
-            onClick={handleDownvote}>{`${downvoted? "  ":"keyboard_arrow_down"}`}</i>
-            {/* <img
+  return (
+    <div className="IconsBoxPost">
+      <div className="PostUpBox">
+        <i className="material-icons" onClick={handleUpvote}>{`${
+          upvoted ? "  " : "keyboard_arrow_up"
+        }`}</i>
+      </div>
+      <div className="PostCountBox">
+        <p>
+          <img
+            className="LightbulbIcon"
+            src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595186577/Untitled_8_1_ymtxrr.png"
+            alt="whole lightbulb"
+          />
+          <strong>{` `} {votes}</strong>
+        </p>
+      </div>
+      <div className="PostDownBox">
+        <i className="material-icons" onClick={handleDownvote}>{`${
+          downvoted ? "  " : "keyboard_arrow_down"
+        }`}</i>
+        {/* <img
                 className="LightbulbIcon"
                 src="https://res.cloudinary.com/dgmpgmo60/image/upload/v1595188165/Untitled_8_2_glwnue.png"
                 alt="Broken lightbulb"
             /> */}
-            </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default Votes;
